18b701f0561af77cba7faf9a0123e904fb7d9535
wangxy-github <1710050555@qq.com>
2023-05-20 14:48:21 +0800

    原型模式

---
+++ src/main/java/single/wxy/type3_principle/Sheep.java
@@ -0,0 +1,50 @@
+package single.wxy.type3_principle;
+
+/**
+ * @author wxy
+ * @date: 2023/5/20 2:09 PM
+ * @ClassName: Sheep
+ */
+
+public class Sheep implements Cloneable{
+    private String name;
+    private int age;
+    private String color;
+    private String address = "蒙古羊";
+    public Sheep friend; //是对象, 克隆是会如何处理, 默认是浅拷贝
+
+    public Sheep(String name, int age, String color) {
+        super();
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Sheep{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", color='" + color + '\'' +
+                ", address='" + address + '\'' +
+                ", friend=" + friend +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Sheep sheep = null;
+        try {
+            sheep = (Sheep)super.clone();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return sheep;
+    }
+
+}
